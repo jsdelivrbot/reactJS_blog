ae5330d480f0e34e2b317383661b9f0cb7584d35
TalhaTZA <talhavjyza@gmail.com>
2018-01-22 22:09:49 +0500

    ADD list of posts

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -12,7 +12,8 @@ class PostsIndex extends Component{
         <div className='text-xs-right'>
           <Link to='/posts/new' className='btn btn-primary'>ADD NEW POST</Link>
         </div>
-        List of blog posts
+        <h3>Posts</h3>
+        <ul className='list-group'>{this.renderPost()}</ul>
         </div>
     );
   }
@@ -21,10 +22,25 @@ class PostsIndex extends Component{
     this.props.fetchPosts();
   }
 
+  renderPost(){
+    return this.props.posts.map((post)=>{
+        return (
+        <li className='list-group-item' key={post.id}>
+          <strong>{post.title}</strong>
+          <span className='pull-xs-right'>{post.categories}</span>
+        </li>
+        );
+    });
+  }
+
 }
 
 // function mapDispatchToProps(dispatch){
 //   return bindActionCreators({fetchPosts},dispatch);
 // }
 
-export default connect(null,{fetchPosts:fetchPosts})(PostsIndex);
+function mapStateToProps(state){
+  return {posts: state.posts.all};
+}
+
+export default connect(mapStateToProps,{fetchPosts:fetchPosts})(PostsIndex);
